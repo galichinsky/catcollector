b01b113b2ddc623fdf89e38c52caffeeb57f31ad
Jim Clark <jim.clark@generalassemb.ly>
2024-09-27 09:19:07 -0700

    Complete home page (you do)

--- main_app/views.py
+++ main_app/views.py
@@ -18,7 +18,7 @@ cats = [
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
